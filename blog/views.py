3d2ace8dd248db4805365e76e1b21956fa889ce4
mark <mark.jeon@bluetilelab.com>
2018-12-19 20:14:24 +0900

    Modify and Create bug fix.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -21,7 +21,7 @@ def post_new(request):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm()
+        form = PostForm(request.POST)
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
